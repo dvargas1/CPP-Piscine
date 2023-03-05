011d53ba391521151bb0b431aad6a987e7961c61
dvargas1 <daniel_v93@hotmail.com>
2023-03-05 08:01:55 -0300

    ex02

--- CPP-01/ex02/main.cpp
+++ CPP-01/ex02/main.cpp
@@ -1,6 +1,26 @@
 #include <string>
-#include <stdlib.h>
+#include <iostream>
 
 int main(void){
+
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout<<"-- Vamos printar os endereços -- "<<std::endl;
+	std::cout<<"Endereço da string"<<std::endl;
+	std::cout<<&str<<std::endl;
+	std::cout<<"Endereço em que o ponteiro aponta"<<std::endl;
+	std::cout<<stringPTR<<std::endl;
+	std::cout<<"Endereço da referencia"<<std::endl;
+	std::cout<<&stringREF<<std::endl;
+	std::cout<<"\n\n-- Vamos printar os valores -- "<<std::endl;
+	std::cout<<"Valor da string"<<std::endl;
+	std::cout<<str<<std::endl;
+	std::cout<<"Valor em que o ponteiro aponta"<<std::endl;
+	std::cout<<*stringPTR<<std::endl;
+	std::cout<<"Valor referenciado"<<std::endl;
+	std::cout<<stringREF<<std::endl;
+
 	return(0);
 }
